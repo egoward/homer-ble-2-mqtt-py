fc29945c023b8cc058ebdc515094289b44e1424b
Ed Goward <edward.goward@animated-insights.com>
2021-11-13 07:55:11 +0000

    Add MQTT auto-reconnect logic

--- ble2mqtt.py
+++ ble2mqtt.py
@@ -1,49 +1,80 @@
-import sys, json, socket, asyncio
+import sys, json, socket, asyncio, time
 from bleak import BleakScanner
-from asyncio_mqtt import Client
+import asyncio_mqtt
+import paho.mqtt.client as mqtt
+
 
 if sys.platform == "win32":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-async def detection_callback(device, advertisement_data):
-    global mqtt, hostname, counter
+class BLE2MQTT:
+  def __init__(self):
+    self.x=3
+    print("Hello")
+    self.counter = 0
+    self.hostname = socket.gethostname()
+    self.conn = asyncio_mqtt.Client("House")
+    self.last_connect_time = 0
+    self.reconnect_interval_seconds = 10
+    self.heartbeat_interval_seconds = 60
+    self.scanner = BleakScanner(scanning_mode='passive')
+    self.scanner.register_detection_callback(self.detection_callback)
+  
+  async def start(self):
+    await self.maybe_connect()
+    asyncio.get_event_loop().create_task(self.heartbeat())
+    await self.scanner.start()
 
-    manufacturer = []
-    for key, value in advertisement_data.manufacturer_data.items():
-        manufacturer.append( {'company':key, 'data':value.hex()} )
-    service = {}
-    for key, value in advertisement_data.service_data.items():
-        service[ key ] = value.hex()
-    obj4json = {
-        'name':device.name,
-        'manufacturer' : manufacturer,
-        'service' : service,
-        'service_uuids' : advertisement_data.service_uuids
-    }
-    jsonText = json.dumps(obj4json)
-    await mqtt.publish("/ble/" + hostname + "/device/" + device.address,jsonText)
-    counter = counter + 1
+  async def maybe_connect(self):
+    if time.time() - self.last_connect_time < self.reconnect_interval_seconds:
+      return  
+    print("Connecting to MQTT")
+    try:
+      self.last_connect_time = time.time()
+      await self.conn.connect()
+      print("Connected")
+    except BaseException as eConn:
+      print("Error Connecting : ", eConn)
+
+  async def try_send(self, subject, message):
+    print("Message for ", subject, " : " , message)
+    try:
+      await self.conn.publish(subject, message)
+    except  asyncio_mqtt.error.MqttCodeError as ePub:
+      print("Error sending message :  ", ePub)
+      if ePub.rc is mqtt.MQTT_ERR_NO_CONN:
+        await self.maybe_connect()
+
+  async def detection_callback(self,device, advertisement_data):
+      global conn, hostname, counter
+      manufacturer = []
+      for key, value in advertisement_data.manufacturer_data.items():
+          manufacturer.append( {'company':key, 'data':value.hex()} )
+      service = {}
+      for key, value in advertisement_data.service_data.items():
+          service[ key ] = value.hex()
+      obj4json = {
+          'name':device.name,
+          'manufacturer' : manufacturer,
+          'service' : service,
+          'service_uuids' : advertisement_data.service_uuids
+      }
+      jsonText = json.dumps(obj4json)
+      await self.try_send("/ble/" + self.hostname + "/device/" + device.address,jsonText)
+      self.counter = self.counter + 1
+
+  async def heartbeat(self):
+    while True:
+      await self.try_send("/ble/"+self.hostname+"/status",json.dumps({'sent':self.counter}))
+      await asyncio.sleep(self.heartbeat_interval_seconds)
+
 
 async def main():
-    global mqtt, hostname, counter
-    counter = 0
-    hostname = socket.gethostname()
+    ble2mqtt = BLE2MQTT()
+    await ble2mqtt.start()
 
-    mqtt = Client("house")
-    scanner = BleakScanner(scanning_mode='passive')
-
-    await mqtt.connect()
-
-    await mqtt.publish("/ble/bridgerunning","yes")
-
-    scanner.register_detection_callback(detection_callback)
-
-    await scanner.start()
     while True:
-        print("Sent BLE messages", counter)
-        await asyncio.sleep(5.0)
+        print("Main loop ticking away")
+        await asyncio.sleep(60.0)
 
-    #This never happens!
-    await scanner.stop()
-   
 asyncio.run(main())
--- readme.md
+++ readme.md
@@ -8,27 +8,27 @@ Designed to run on random devices around the house so BLE range for temperature 
 
 # Considerations
 
-We're just relaying one bus to another but that's a bit interesting from a privacy standpoint.
+We're just relaying one bus to another but that's a bit interesting from a privacy standpoint given anyone's data can show up.
 
 If the messages are going to go further or be persisted messages, you need to whitelist device IDs or you're probably breaking data protection laws.
 
 # How do you use it?
 
-On Windows, onstall Python 3.X from MS store, something like:
+On Windows, install Python 3.X from MS store, on linux you could run:
 
-
+```
 sudo apt-get install python3-pip
-
-```
-sudo pip3 install bleak asyncio_mqtt
 ```
 
-Make sure you run it with python3
-Change the MQTT connection parameters to ones that work for you.
+Then:
+```
+pip3 install bleak asyncio_mqtt
+```
+
+Make sure you run it with python3.  Change the MQTT connection parameters to ones that work for you.
 
 
-
-Give the 'pi' user access to bluetooth
+On a pi, give the 'pi' user access to bluetooth
 ```
 sudo usermod -a -G bluetooth pi
 sudo systemctl restart bluetooth
@@ -36,7 +36,7 @@ sudo systemctl restart dbus
 ```
 
 
-And/or installation as a service (which runs as root)
+And/or installation as a service (which runs as root).  Something like:
 ```
 sudo cp ble2mqtt.service /etc/systemd/system/
 sudo systemctl daemon-reload
